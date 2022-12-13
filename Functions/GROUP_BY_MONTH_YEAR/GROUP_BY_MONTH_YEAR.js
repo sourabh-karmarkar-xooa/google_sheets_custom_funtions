025123db88a6bba8435d359e89cf71c5bd84d297
sourabh-karmarkar-xooa <sourabh.karmarkar@xoaa.com>
2022-12-13 19:46:53 +0530

    Added minor corrections and refactoring

--- Functions/GROUP_BY_MONTH_YEAR/GROUP_BY_MONTH_YEAR.js
+++ Functions/GROUP_BY_MONTH_YEAR/GROUP_BY_MONTH_YEAR.js
@@ -1,17 +1,17 @@
 /**
  * Returns a list grouped by the month and year combination specified in the group_by_range parameter along with the addition/sum of values from group_by_values parameter.
- * If the result needs to be filtered further specify the collumn number in the filter_col_number parameter along with the value that needs to be matched in the filter_text parameter.
+ * If the result needs to be filtered further specify the column number in the filter_col_number parameter along with the list of values separated by a comma that need to be matched in the filter_text parameter.
  *
  * @param {Range} range The full data range on which the grouping operation is applied.
  * @param {Range} group_by_range The range whose values are considered for grouping.
  * @param {Range} group_by_values The range whose values are considered for addition/sum while grouping.
- * @param {String} filter_text The string value which will be used to include only the results that match the value.
+ * @param {String} filter_text The list of string values separated by a comma which will be used to include only the results that match the value.
  * @param {Number} filter_col_number The column number on which the filter_text string will be matched to include only those results.
  * 
  * @returns {Range} A list of grouped results based on the parameters specified.
  * @customfunction
  */
- function GROUP_BY_MONTH_YEAR(range, group_by_range, group_by_values, filter_text, filter_col_number) {
+function GROUP_BY_MONTH_YEAR(range, group_by_range, group_by_values, filter_text, filter_col_number) {
 
   let errorMessage = '';
 
@@ -32,10 +32,14 @@
       const record = range[i];
       const date = group_by_range[i][0];
       if (!date) continue;
+
       if (filter_text && filter_col_number) {
-
-        if (record[filter_col_number - 1] !== filter_text) continue;
-
+        const brokersList = filter_text.split(",");
+        let considerRowToCalculate = false;
+        for (const value of brokersList) {
+          if (record[filter_col_number - 1] === value) considerRowToCalculate = true;
+        }
+        if(!considerRowToCalculate) continue;
       }
 
       const monthYear = new Date(date).getMonth() + "-" + new Date(date).getFullYear();
@@ -66,7 +70,8 @@
     }
 
     if (result.length === 0) return "";
-    return result;
+    
+    return result.reverse();
 
   } catch (error) {
 
